491329ea21bc5a9547f84e3134b739e4ee35090f
achanont <johndoe@example.com>
2018-12-24 01:54:15 -0600

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-]
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #Detail Page - URL pointing Django to a view named post_detail, that will show an entire blog post.
+    # post/<int:pk>/  means that Django expects an integer value and will transfer it to a view as a variable called pk.
 
-urlpatterns += staticfiles_urlpatterns()
+]
+# test
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post #import model post (from models.py) so that we can include code from different files
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def post_list(request):
+    #created a QuerySet named 'posts'
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #'blog/post_list.html' refers to a template file
+    #parameter in {} needs a name so that we can allow templates to use
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# post's detail view
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
